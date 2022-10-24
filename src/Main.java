534f7a5feb8b5f21870a37238751c0ad5911826a
Jossemand <s224564@student.dtu.dk>
2022-10-24 17:39:17 +0200

    Commit test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! Is this going to git???");
+        System.out.println("Hello world! Is this going to git??");
     }
 }
