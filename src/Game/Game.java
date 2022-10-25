a936e1f40574d3a1283b8a7981e902f256ff001e
Philip <philip.tran@hotmail.dk>
2022-10-25 17:40:10 +0100

    sout to test scanner

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -21,6 +21,7 @@ public class Game {
     private void createPlayer() {
 
         player_1 = new Player();
+        System.out.println("Player 1 name is");
         player_2 = new Player();
     }
 
